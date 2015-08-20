6b4f4b115f2426d80e3d53db970bf339f77f40a2
Sam Power <ejidiong@gmail.com>
2015-08-20 12:04:01 +0100

    clean up index page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  # acts_as_avatarable
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -16,6 +16,8 @@ class User < ActiveRecord::Base
   :uniqueness => {
     :case_sensitive => false
   }
+
+  acts_as_avatarable
   
   attr_accessor :login
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150818174628) do
+ActiveRecord::Schema.define(version: 20150820094553) do
 
   create_table "books", force: :cascade do |t|
     t.string   "link"
@@ -51,6 +51,10 @@ ActiveRecord::Schema.define(version: 20150818174628) do
     t.string   "username"
     t.datetime "date_of_birth"
     t.boolean  "is_female",              default: false
+    t.string   "logo_file_name"
+    t.string   "logo_content_type"
+    t.integer  "logo_file_size"
+    t.datetime "logo_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
